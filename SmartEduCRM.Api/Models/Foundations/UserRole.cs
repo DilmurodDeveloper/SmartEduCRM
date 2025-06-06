439e670bd1256763b5c8c6ac8542a145a57d1b2a
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-06-06 23:43:16 +0500

    DATA: Model & Migrate Users

---
+++ SmartEduCRM.Api/Models/Foundations/UserRole.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartEduCRM.Api.Models.Foundations
+{
+    public enum UserRole
+    {
+        Admin,
+        Operator,
+        Marketing,
+        Teacher,
+        Accountant
+    }
+}
