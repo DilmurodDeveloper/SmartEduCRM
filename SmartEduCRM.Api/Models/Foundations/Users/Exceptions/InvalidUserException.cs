bf2c63f55be8f0e012d5d210b7936f14a2a9940f
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-06-07 20:47:22 +0500

    ShouldThrowValidationExceptionOnAddIfUserIsInvalidAndLogItAsync -> FAIL

---
+++ SmartEduCRM.Api/Models/Foundations/Users/Exceptions/InvalidUserException.cs
@@ -0,0 +1,11 @@
+﻿using Xeptions;
+
+namespace SmartEduCRM.Api.Models.Foundations.Users.Exceptions
+{
+    public class InvalidUserException : Xeption
+    {
+        public InvalidUserException()
+            : base(message: "User is invalid. Please fix the errors and try again")
+        { }
+    }
+}
--- SmartEduCRM.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Add.cs
+++ SmartEduCRM.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Add.cs
@@ -35,5 +35,65 @@ namespace SmartEduCRM.Tests.Unit.Services.Foundations.Users
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfUserIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidUser = new User
+            {
+                FirstName = invalidText,
+            };
+
+            var invalidUserException = new InvalidUserException();
+
+            invalidUserException.AddData(
+                key: nameof(User.Id),
+                values: "Id is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.FirstName),
+                values: "Text is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.LastName),
+                values: "Text is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.Email),
+                values: "Text is required");
+
+            invalidUserException.AddData(
+                key: nameof(User.PasswordHash),
+                values: "Text is required");
+
+            var expectedUserValidationException =
+                new UserValidationException(invalidUserException);
+            
+            // when
+            ValueTask<User> addUserTask =
+                this.userService.AddUserAsync(invalidUser);
+            
+            // then
+            await Assert.ThrowsAsync<UserValidationException>(() =>
+                addUserTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedUserValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertUserAsync(It.IsAny<User>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }
