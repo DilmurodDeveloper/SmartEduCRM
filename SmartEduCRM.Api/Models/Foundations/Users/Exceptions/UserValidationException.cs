b3d71a7479cb192cb8e904f7df9fbd8677d9cc48
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-06-07 15:33:21 +0500

    ShouldThrowValidationExceptionOnAddIfUserIsNullAndLogItAsync -> WIP

---
+++ SmartEduCRM.Api/Models/Foundations/Users/Exceptions/UserValidationException.cs
@@ -0,0 +1,12 @@
+﻿using Xeptions;
+
+namespace SmartEduCRM.Api.Models.Foundations.Users.Exceptions
+{
+    public class UserValidationException : Xeption
+    {
+        public UserValidationException(Xeption innerException)
+            : base(message: "User validation error occurred, fix the errors and try again", 
+                  innerException)
+        { }
+    }
+}
---
+++ SmartEduCRM.Tests.Unit/Services/Foundations/Users/UserServiceTests.Validations.Add.cs
@@ -0,0 +1,26 @@
+﻿using SmartEduCRM.Api.Models.Foundations.Users;
+using SmartEduCRM.Api.Models.Foundations.Users.Exceptions;
+
+namespace SmartEduCRM.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfUserIsNullAndLogItAsync()
+        {
+            // given
+            User nullUser = null;
+            var nullUserException = new NullUserException();
+            var expectedUserValidationException =
+                new UserValidationException(nullUserException);
+
+            // when
+            ValueTask<User> addUserTask =
+                this.userService.AddUserAsync(nullUser);
+
+            // then
+            await Assert.ThrowsAsync<UserValidationException>(() =>
+                addUserTask.AsTask());
+        }
+    }
+}
